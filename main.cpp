5b581367d6b75fc5f771ee1e34c039e7ecf9824e
Dmytro Varchuk <loltop792@gmail.com>
2025-01-04 17:59:08 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int* a = new int;
+
+    cin >> *a;
+    cout << (*a == 0 ? "zero" : (*a < 0 ? "negative" : "positive")) << endl;
+
+    if (a != nullptr)
+    {
+        delete a;
+    }
+    return 0;
+}
