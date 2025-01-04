33d5ef054e2cf72482be0383665e121889e82d29
Dmytro Varchuk <loltop792@gmail.com>
2025-01-04 17:57:31 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int * a = new int;
+    int * b = new int;
+
+    cout << "Enter value for a: ";
+    cin >> *a;
+    cout << "Enter value for b: ";
+    cin >> *b;
+    cout << (*a > *b ? *a : *b) << endl;
+
+    if (a != nullptr & b != nullptr) {
+        delete a, b;
+    }
+    return 0;
+}
